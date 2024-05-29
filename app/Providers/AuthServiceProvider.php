7b8feac3c03a14e7c81841034ba0cf2c2f49a956
Martin Elias Simarra Salgado <msimarra@cotecmar.com>
2024-05-29 16:34:32 -0500

    a

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+//use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Fortify;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+
+        
+
+        Fortify::authenticateUsing(function ($request) {
+            $validated = Auth::validate([
+                'samaccountname' => $request->username,
+                'password' => $request->password
+            ]);
+            
+            return $validated ? Auth::getLastAttempted() : null;
+        });
+
+    }
+}
